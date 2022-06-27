4f991973d5352c922c8b1f9a116885f79dba6f4c
Praveen Thamindu <praveenthamindu@gmail.com>
2022-06-27 21:07:28 +0530

    add footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HeroSection from './components/pages/homepage/HeroSection';
 import MyWorkSection from './components/pages/homepage/MyWorkSection';
 import AboutMe from './components/pages/homepage/AboutMe';
 import ContactMe from './components/pages/homepage/ContactMe';
+import Footer from './components/pages/Footer';
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <MyWorkSection/>
       <AboutMe/>
       <ContactMe/>
+      <Footer/>
     </div>
   );
 }
--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -1,7 +1,17 @@
+import "../../style/css/page/footer.css";
+
 const Footer = (props) => {
     return ( 
-        <footer>
-            <p>footer</p>
+        <footer className="footer">
+            <div className="container">
+                <ul className="links">
+                    <li className="link"><a href="">HOME</a></li>
+                    <li className="link"><a href="">MY WORK</a></li>
+                    <li className="link"><a href="">ABOUT ME</a></li>
+                    <li className="link"><a href="">CONTACT</a></li>
+                </ul>
+                <p className="copyright">Design + build by Thamindu Praveen ⓒ2022</p>
+            </div>
         </footer>
      );
 }
