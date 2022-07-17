ce5953bb365b70769e0735840874a61855fdaad0
Praveen Thamindu <praveenthamindu@gmail.com>
2022-07-17 20:39:47 +0530

    add style to mywork page mobile version

--- src/components/pages/mywork/MyWorkPage.jsx
+++ src/components/pages/mywork/MyWorkPage.jsx
@@ -1,120 +1,125 @@
 import BounceLetter from "../../common/BounceLetter";
+import Footer from "../Footer";
 import "../../../style/css/page/mywork/myWorkPage.css";
 
 const MyWorkPage = (props) => {
     return ( 
-        <div className="mywork">
-            <h1 className="h1"><BounceLetter>My</BounceLetter> <BounceLetter>Work</BounceLetter></h1>
-            <p className="discription">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti dolor pariatur aliquam laborum velit accusamus ab, saepe ipsam debitis, impedit quod ullam ut nesciunt alias distinctio possimus minima. Obcaecati, impedit!
-            </p>
-            <div className="col">
-                <div className="row">
-                    <div className="thumbnail">
-                        <img src="./images/urlshorten.jpg" alt="" className="img"/>
-                        <a href="https://www.github.com" target="_blank" rel="noreferrer" className="link code-link" title="code">
-                            <i className="fa-solid fa-code"></i>
-                        </a>
-                        <a href="https://www.google.com" target="_blank" rel="noreferrer" className="link live-link" title="live">
-                            <i className="fa-solid fa-desktop"></i>
-                        </a>
+        <>
+            <div className="mywork">
+                <h1 className="h1"><BounceLetter>My</BounceLetter> <BounceLetter>Work</BounceLetter></h1>
+                <p className="discription">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti dolor pariatur aliquam laborum velit accusamus ab, saepe ipsam debitis, impedit quod ullam ut nesciunt alias distinctio possimus minima. Obcaecati, impedit!
+                </p>
+                <div className="col">
+                    <div className="row">
+                        <div className="thumbnail">
+                            <img src="./images/urlshorten.jpg" alt="" className="img"/>
+                            <a href="https://www.github.com" target="_blank" rel="noreferrer" className="link code-link" title="code">
+                                <i className="fa-solid fa-code"></i>
+                            </a>
+                            <a href="https://www.google.com" target="_blank" rel="noreferrer" className="link live-link" title="live">
+                                <i className="fa-solid fa-desktop"></i>
+                            </a>
+                        </div>
+                        <div className="details">
+                            <p className="details-title">Title</p>
+                            <p className="details-discription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempore, distinctio fugit molestiae rerum reiciendis commodi officia impedit error repudiandae molestias labore minima amet. Sunt?</p>
+                            <ul className="details-ul">
+                                <li className="details-ul_li">HTML</li>
+                                <li className="details-ul_li">CSS</li>
+                                <li className="details-ul_li">javaScript</li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="details">
-                        <p className="details-title">Title</p>
-                        <p className="details-discription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempore, distinctio fugit molestiae rerum reiciendis commodi officia impedit error repudiandae molestias labore minima amet. Sunt?</p>
-                        <ul className="details-ul">
-                            <li className="details-ul_li">HTML</li>
-                            <li className="details-ul_li">CSS</li>
-                            <li className="details-ul_li">javaScript</li>
-                        </ul>
-                    </div>
-                </div>
 
-                <div className="row">
-                    <div className="thumbnail">
-                        <img src="./images/countryapi.jpg" alt="" className="img"/>
-                        <a href="https://www.github.com" target="_blank" rel="noreferrer" className="link code-link" title="code">
-                            <i className="fa-solid fa-code"></i>
-                        </a>
-                        <a href="https://www.google.com" target="_blank" rel="noreferrer" className="link live-link" title="live">
-                            <i className="fa-solid fa-desktop"></i>
-                        </a>
+                    <div className="row">
+                        <div className="thumbnail">
+                            <img src="./images/countryapi.jpg" alt="" className="img"/>
+                            <a href="https://www.github.com" target="_blank" rel="noreferrer" className="link code-link" title="code">
+                                <i className="fa-solid fa-code"></i>
+                            </a>
+                            <a href="https://www.google.com" target="_blank" rel="noreferrer" className="link live-link" title="live">
+                                <i className="fa-solid fa-desktop"></i>
+                            </a>
+                        </div>
+                        <div className="details">
+                            <p className="details-title">Title</p>
+                            <p className="details-discription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempore, distinctio fugit molestiae rerum reiciendis commodi officia impedit error repudiandae molestias labore minima amet. Sunt?</p>
+                            <ul className="details-ul">
+                                <li className="details-ul_li">HTML</li>
+                                <li className="details-ul_li">CSS</li>
+                                <li className="details-ul_li">javaScript</li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="details">
-                        <p className="details-title">Title</p>
-                        <p className="details-discription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempore, distinctio fugit molestiae rerum reiciendis commodi officia impedit error repudiandae molestias labore minima amet. Sunt?</p>
-                        <ul className="details-ul">
-                            <li className="details-ul_li">HTML</li>
-                            <li className="details-ul_li">CSS</li>
-                            <li className="details-ul_li">javaScript</li>
-                        </ul>
-                    </div>
-                </div>
 
-                <div className="row">
-                    <div className="thumbnail">
-                        <img src="./images/netflix.jpg" alt="" className="img"/>
-                        <a href="https://www.github.com" target="_blank" rel="noreferrer" className="link code-link" title="code">
-                            <i className="fa-solid fa-code"></i>
-                        </a>
-                        <a href="https://www.google.com" target="_blank" rel="noreferrer" className="link live-link" title="live">
-                            <i className="fa-solid fa-desktop"></i>
-                        </a>
+                    <div className="row">
+                        <div className="thumbnail">
+                            <img src="./images/netflix.jpg" alt="" className="img"/>
+                            <a href="https://www.github.com" target="_blank" rel="noreferrer" className="link code-link" title="code">
+                                <i className="fa-solid fa-code"></i>
+                            </a>
+                            <a href="https://www.google.com" target="_blank" rel="noreferrer" className="link live-link" title="live">
+                                <i className="fa-solid fa-desktop"></i>
+                            </a>
+                        </div>
+                        <div className="details">
+                            <p className="details-title">Title</p>
+                            <p className="details-discription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempore, distinctio fugit molestiae rerum reiciendis commodi officia impedit error repudiandae molestias labore minima amet. Sunt?</p>
+                            <ul className="details-ul">
+                                <li className="details-ul_li">HTML</li>
+                                <li className="details-ul_li">CSS</li>
+                                <li className="details-ul_li">javaScript</li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="details">
-                        <p className="details-title">Title</p>
-                        <p className="details-discription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempore, distinctio fugit molestiae rerum reiciendis commodi officia impedit error repudiandae molestias labore minima amet. Sunt?</p>
-                        <ul className="details-ul">
-                            <li className="details-ul_li">HTML</li>
-                            <li className="details-ul_li">CSS</li>
-                            <li className="details-ul_li">javaScript</li>
-                        </ul>
-                    </div>
-                </div>
 
-                <div className="row">
-                    <div className="thumbnail">
-                        <img src="./images/todoapp.jpg" alt="" className="img"/>
-                        <a href="https://www.github.com" target="_blank" rel="noreferrer" className="link code-link" title="code">
-                            <i className="fa-solid fa-code"></i>
-                        </a>
-                        <a href="https://www.google.com" target="_blank" rel="noreferrer" className="link live-link" title="live">
-                            <i className="fa-solid fa-desktop"></i>
-                        </a>
+                    <div className="row">
+                        <div className="thumbnail">
+                            <img src="./images/todoapp.jpg" alt="" className="img"/>
+                            <a href="https://www.github.com" target="_blank" rel="noreferrer" className="link code-link" title="code">
+                                <i className="fa-solid fa-code"></i>
+                            </a>
+                            <a href="https://www.google.com" target="_blank" rel="noreferrer" className="link live-link" title="live">
+                                <i className="fa-solid fa-desktop"></i>
+                            </a>
+                        </div>
+                        <div className="details">
+                            <p className="details-title">Title</p>
+                            <p className="details-discription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempore, distinctio fugit molestiae rerum reiciendis commodi officia impedit error repudiandae molestias labore minima amet. Sunt?</p>
+                            <ul className="details-ul">
+                                <li className="details-ul_li">HTML</li>
+                                <li className="details-ul_li">CSS</li>
+                                <li className="details-ul_li">javaScript</li>
+                            </ul>
+                        </div>
                     </div>
-                    <div className="details">
-                        <p className="details-title">Title</p>
-                        <p className="details-discription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempore, distinctio fugit molestiae rerum reiciendis commodi officia impedit error repudiandae molestias labore minima amet. Sunt?</p>
-                        <ul className="details-ul">
-                            <li className="details-ul_li">HTML</li>
-                            <li className="details-ul_li">CSS</li>
-                            <li className="details-ul_li">javaScript</li>
-                        </ul>
-                    </div>
-                </div>
 
-                <div className="row">
-                    <div className="thumbnail">
-                        <img src="./images/loopstudio.jpg" alt="" className="img"/>
-                        <a href="https://www.github.com" target="_blank" rel="noreferrer" className="link code-link" title="code">
-                            <i className="fa-solid fa-code"></i>
-                        </a>
-                        <a href="https://www.google.com" target="_blank" rel="noreferrer" className="link live-link" title="live">
-                            <i className="fa-solid fa-desktop"></i>
-                        </a>
-                    </div>
-                    <div className="details">
-                        <p className="details-title">Title</p>
-                        <p className="details-discription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempore, distinctio fugit molestiae rerum reiciendis commodi officia impedit error repudiandae molestias labore minima amet. Sunt?</p>
-                        <ul className="details-ul">
-                            <li className="details-ul_li">HTML</li>
-                            <li className="details-ul_li">CSS</li>
-                            <li className="details-ul_li">javaScript</li>
-                        </ul>
+                    <div className="row">
+                        <div className="thumbnail">
+                            <img src="./images/loopstudio.jpg" alt="" className="img"/>
+                            <a href="https://www.github.com" target="_blank" rel="noreferrer" className="link code-link" title="code">
+                                <i className="fa-solid fa-code"></i>
+                            </a>
+                            <a href="https://www.google.com" target="_blank" rel="noreferrer" className="link live-link" title="live">
+                                <i className="fa-solid fa-desktop"></i>
+                            </a>
+                        </div>
+                        <div className="details">
+                            <p className="details-title">Title</p>
+                            <p className="details-discription">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum tempore, distinctio fugit molestiae rerum reiciendis commodi officia impedit error repudiandae molestias labore minima amet. Sunt?</p>
+                            <ul className="details-ul">
+                                <li className="details-ul_li">HTML</li>
+                                <li className="details-ul_li">CSS</li>
+                                <li className="details-ul_li">javaScript</li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <Footer/>
+        </>
+
      );
 }
  
