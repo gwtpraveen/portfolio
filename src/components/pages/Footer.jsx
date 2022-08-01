5d5495cf818aa99b47d77a313ffa34fd66894896
Praveen Thamindu <praveenthamindu@gmail.com>
2022-08-01 20:18:10 +0530

    add to top button functionality

--- src/components/pages/Footer.jsx
+++ src/components/pages/Footer.jsx
@@ -1,11 +1,18 @@
 import "../../style/css/page/footer.css";
 
 const Footer = (props) => {
+    const handleToTop = () => {
+            window.scrollTo({
+            top: 0, 
+            behavior: 'smooth'
+        });
+    }
+
     return ( 
         <footer className="footer">
             <div className="container">
                 <p className="copyright">Design + build by Thamindu Praveen ⓒ2022</p>
-                <button className="toTop">
+                <button className="toTop" onClick={handleToTop}>
                     <i className="fa-solid fa-angle-up"></i>
                 </button>
             </div>
